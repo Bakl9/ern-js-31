687c3dd42fa2532aa0d8a399a1f162255bd101e4
Makarov ilya <74796903+Bakl9@users.noreply.github.com>
2021-03-17 11:02:58 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+// function marry (man, woman) {
+//     woman.husband = man;
+//     man.wife = woman; 
+
+//     return {
+//         father: man,
+//         mather: woman
+//     }
+// }
+
+//     let famaly = marry ({
+//         name: 'John'
+//     }, {
+//         name: "Ann"
+//     })
+
+//     // delete family. father;
+//     // delete family. mather.husband;
+
+//     alert(famaly.name)
+
+
+// let user = { name:'ilsha', description: {
+//     age: 26,
+//     walls: 'brown'
+// }}
+
+// delete user.description 
+
+// alert(user.description.age)
